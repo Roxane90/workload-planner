2b3b5d123741116fb5cb408d69e4e3dc7cd12235
RoxaneRz <roxane.reza@student.ucll.be>
2026-04-05 15:35:13 +0200

    Spring Boot project setup + DB connection

---
+++ src/test/java/com/roxane/workload_planner/WorkloadPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roxane.workload_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkloadPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
